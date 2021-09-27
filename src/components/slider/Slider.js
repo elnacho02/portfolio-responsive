de679ea784d41c4509b0efabd3b1bace026eb0e7
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-09-27 00:09:50 -0300

    asdasd

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,47 +1,53 @@
 import React from 'react';
-import Carousel from "@brainhubeu/react-carousel";
+import Carousel, { autoplayPlugin, slidesToShowPlugin } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
 import Slides from "./Slides";
 import "./Slider.css";
-import { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+
 
 const Slider = () => {
     
     return (
         <div className="carousel-container" id="projects">
-            <div className="carousel-title">
+            <div className="carousel-title" >
                 <h2>My Projects</h2>
             </div>
 
              <Carousel 
-                plugins={[
-                    'arrows','centered',
-                    {
-                      resolve: slidesToShowPlugin,
-                      options: {
-                       numberOfSlides: 2
-                      }
+                
+                plugins={["centered","arrows",
+                  
+                  {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                     numberOfSlides: 2
                     }
-                  ]}
-                animationSpeed={200}
-                offset={50}
-                itemWidth={350}
-                slides={Slides}
+                  },
+                  
+                ]}
+                offset={20}
                 breakpoints={{
-                    960:{
-                        plugins: ["centered","infinite",
-                            {
-                              resolve: slidesToShowPlugin,
-                              options: {
-                               numberOfSlides: 4
-                              }
-                            },
-                          ],
-                        itemWidth: 250 
-                        
-                    }
-                }}
-            /> 
+                  960:{
+                      plugins: ['centered',
+                          {
+                            resolve: slidesToShowPlugin,
+                            options: {
+                             numberOfSlides: 1
+                            }
+                          },
+                          {
+                            resolve: autoplayPlugin,
+                            options: {
+                              interval: 4000,
+                            }
+                          },
+                        ],
+                  }
+                  
+              }}
+              slides={Slides}  
+                
+                /> 
             
             
             
