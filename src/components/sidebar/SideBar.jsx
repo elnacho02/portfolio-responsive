2c807e6ff2322afd36ee2b24fb5b4ee47f17fbc8
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-09-29 16:29:32 -0300

    asdasd

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -6,6 +6,11 @@ const Info = () => {
     const seleccionar= (x)=>{
         setSelected(x)
     }
+    const seleccionarIcon=(x)=>{
+        setSkillSelected(x)
+    }
+
+    var [skillSelected, setSkillSelected] = useState("jsStars")
     
     return (
             <div className="sidebarContainer">
@@ -32,15 +37,71 @@ const Info = () => {
                             <br />
                             <p className="mystackP">
                                 I built my stack at Henry's bootcamp, with my skills focused on front-end development, but without leaving behind back-end to consolidate a complete web development base. <br/>
-                                Here what I can use:
+                                 Using Javascript, HTML5, CSS and frameworks/libraries like React, React-Redux, Bootstrap
                             </p>
+                            <div class="skillsContainer">
+                                
+                                <div className="progrLeng">
+                                <i  onClick={()=>seleccionarIcon("jsStars")} class={skillSelected==="jsStars" ? "fab fa-js jsSelected" : "fab fa-js js" } ></i>
+                                <i  onClick={()=>seleccionarIcon("htmlStars")} class={skillSelected==="htmlStars" ? "fab fa-html5 html5Selected" : "fab fa-html5 html5" } ></i>
+                                <i  onClick={()=>seleccionarIcon("cssStars")} class={skillSelected==="cssStars" ? "fab fa-css3 css3Selected" : "fab fa-css3 css3" } ></i>
+                                <i  onClick={()=>seleccionarIcon("reactStars")} class={skillSelected==="reactStars" ? "fab fa-react reactSelected" : "fab fa-react react" } ></i>
+                                <i  onClick={()=>seleccionarIcon("wordpressStars")} class={skillSelected==="wordpressStars" ? "fab fa-wordpress wordpressSelected" : "fab fa-wordpress wordpress" } ></i>
+
+                                </div>
+                                {skillSelected==="jsStars" &&
+                                <div >
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star "></i>
+                                        <i class="far fa-star"></i>
+                                </div>}
+                                {skillSelected==="htmlStars" &&
+                                <div >
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="far fa-star"></i>
+                                </div>}
+                                {skillSelected==="cssStars" &&
+                                <div >
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star "></i>
+                                        <i class="far fa-star"></i>
+                                </div>}
+                                {skillSelected==="reactStars" &&
+                                <div >
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="far fa-star"></i>
+                                </div>}
+                                {skillSelected==="wordpressStars" &&
+                                <div >
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star "></i>
+                                        <i class="far fa-star"></i>
+                                </div>}
+                             </div>
                             <strong>Lenguages</strong>
                             <hr />
                             <div className="idiomasLevel">
-                                <span className="flag-icon flag-icon-us"></span><span className="span">English Intermediate (B1)</span>
-                                <span className="flag-icon flag-icon-br"></span><span className="span">Portugues Basic (A2)</span>
-                                <span className="flag-icon flag-icon-es"></span><span className="span">Spanish Native</span>
-
+                                <div className="banderaLevel">
+                                    <span className="flag-icon flag-icon-us"></span><span className="span">English Intermediate (B1)</span>
+                                </div>
+                                <div className="banderaLevel">
+                                    <span className="flag-icon flag-icon-br"></span><span className="span">Portugues Basic (A2)</span>
+                                </div>
+                                <div className="banderaLevel">
+                                    <span className="flag-icon flag-icon-es"></span><span className="span">Spanish Native</span>
+                                </div>
                             </div>
                         </div>}
                 </div>
